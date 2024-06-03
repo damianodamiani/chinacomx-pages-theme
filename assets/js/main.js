4655479806e68f060ec43b892958743a6b708700
chinacomx <chinacomx.erc@gmail.com>
2024-06-03 20:09:37 +0200

    updates to menu vis

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
     window.addEventListener("scroll", function() {
         if (window.scrollY > window.innerHeight) {
-            header.style.display = "block";
+            header.classList.add("show");
         } else {
-            header.style.display = "none";
+            header.classList.remove("show");
         }
     });
 });
