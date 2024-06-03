3dad9372e9805a6fa4479f154b01e24b96d046bb
chinacomx <chinacomx.erc@gmail.com>
2024-06-03 21:01:57 +0200

    updates stule header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 0) {
-            header.classList.add("show");
-        } else {
-            header.classList.remove("show");
-        }
-    });
+    if (document.body.classList.contains('home')) {
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 0) {
+                header.classList.remove("hide");
+                header.classList.add("show");
+            } else {
+                header.classList.remove("show");
+                header.classList.add("hide");
+            }
+        });
+    } else {
+        header.classList.add("show");
+    }
 });
