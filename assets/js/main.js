c6b1d05bbdeb6f904943134047ff3da133002b92
chinacomx <chinacomx.erc@gmail.com>
2024-06-03 21:02:30 +0200

    Revert "updates stule header"
    This reverts commit 3dad9372e9805a6fa4479f154b01e24b96d046bb.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
-    if (document.body.classList.contains('home')) {
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 0) {
-                header.classList.remove("hide");
-                header.classList.add("show");
-            } else {
-                header.classList.remove("show");
-                header.classList.add("hide");
-            }
-        });
-    } else {
-        header.classList.add("show");
-    }
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
 });
