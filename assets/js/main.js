d05ee09ae2619f392782cbac08eacb72c41a691e
chinacomx <chinacomx.erc@gmail.com>
2024-06-03 22:38:50 +0200

    Revert "post test"
    This reverts commit 6301009d93b70783154fba27d456151e3db18a53.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
-    if (document.body.classList.contains('home')) {
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 0) {
-                header.classList.add("show");
-            } else {
-                header.classList.remove("show");
-            }
-        });
-    } else {
-        header.classList.add("show");
-    }
-
-    const leftBtn = document.querySelector('.left-btn');
-    const rightBtn = document.querySelector('.right-btn');
-    const postWrapper = document.querySelector('.post-wrapper');
-    let currentIndex = 0;
-
-    function updateCarousel() {
-        postWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-
-    leftBtn.addEventListener('click', function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            updateCarousel();
-        }
-    });
-
-    rightBtn.addEventListener('click', function() {
-        if (currentIndex < postWrapper.children.length - 1) {
-            currentIndex++;
-            updateCarousel();
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
         }
     });
 });
