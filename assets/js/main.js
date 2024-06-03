ffea598b9e429f5a9fef87ce9eb1f2668373772a
chinacomx <chinacomx.erc@gmail.com>
2024-06-03 20:28:13 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
     window.addEventListener("scroll", function() {
-        if (window.scrollY > window.innerHeight) {
+        if (window.scrollY > 0) {
             header.classList.add("show");
         } else {
             header.classList.remove("show");
