87c1bda106b59665822c9b96270e084ef00eb0bf
chinacomx <chinacomx.erc@gmail.com>
2024-06-05 11:35:10 +0200

    Revert "trying out pages"
    This reverts commit 516f5a82bcbae1e59b7ac04c693bb5ca107aa555.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    AOS.init();
-
     const header = document.getElementById("header");
-    if (document.body.classList.contains('home')) {
-        header.classList.add('hide'); // Hide initially
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 0) {
-                header.classList.add("show");
-                header.classList.remove("hide");
-            } else {
-                header.classList.remove("show");
-                header.classList.add("hide");
-            }
-        });
-    } else {
-        header.classList.add("show");
-    }
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
 });
