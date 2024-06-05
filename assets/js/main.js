3040cf7627590109c96d606757f1a8d19658e5c5
chinacomx <chinacomx.erc@gmail.com>
2024-06-05 11:41:51 +0200

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+
     const header = document.getElementById("header");
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 0) {
-            header.classList.add("show");
-        } else {
-            header.classList.remove("show");
-        }
-    });
+    if (document.body.classList.contains('home')) {
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 0) {
+                header.classList.add("show");
+            } else {
+                header.classList.remove("show");
+            }
+        });
+    } else {
+        header.classList.add("show");
+    }
 });
