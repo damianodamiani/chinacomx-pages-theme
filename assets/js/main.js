516f5a82bcbae1e59b7ac04c693bb5ca107aa555
chinacomx <chinacomx.erc@gmail.com>
2024-06-05 11:34:52 +0200

    trying out pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+
     const header = document.getElementById("header");
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 0) {
-            header.classList.add("show");
-        } else {
-            header.classList.remove("show");
-        }
-    });
+    if (document.body.classList.contains('home')) {
+        header.classList.add('hide'); // Hide initially
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 0) {
+                header.classList.add("show");
+                header.classList.remove("hide");
+            } else {
+                header.classList.remove("show");
+                header.classList.add("hide");
+            }
+        });
+    } else {
+        header.classList.add("show");
+    }
 });
