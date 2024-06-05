f6d1ac340040176a20f7c17d1ba4192ca134c4f1
chinacomx <chinacomx.erc@gmail.com>
2024-06-05 11:26:52 +0200

    Revert "aos for pages"
    This reverts commit 24ebfca0f7d05aa81092c76dae2841aa90ccea57.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    AOS.init();
-
     const header = document.getElementById("header");
-    if (document.body.classList.contains('home')) {
-        header.setAttribute("data-aos", "slide-down");
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 0) {
-                header.classList.add("show");
-            } else {
-                header.classList.remove("show");
-            }
-        });
-    } else {
-        header.classList.add("show");
-    }
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
 });
